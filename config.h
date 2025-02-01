457d443d628a0a23eaef9d53fc46fa620bd7d7d7
PingalPie <pingalpie@proton.me>
2025-02-01 08:57:31 +0530

    patch done hide_vacant_tags

--- config.h
+++ config.h
@@ -35,7 +35,8 @@ static char *colors[][3] = {
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 /* tagging */
-static const char *tags[] = { ">_", "", "", "", "", "", "", "", "" };
+// static const char *tags[] = { ">_", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
